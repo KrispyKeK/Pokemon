4bc925cb93a05a0766f92ad64767b82164b9592b
JJ <infernoj2581@gmail.com>
2017-12-12 09:50:38 -0700

    Added a picture

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -4,18 +4,25 @@ import javax.swing.*;
 import pokemon.controller.PokemonController;
 
 public class PokemonPanel extends JPanel{
+	
 	PokemonController controller;
+	private SpringLayout baseLayout;
+	private JLabel pokeBall;
+	
 	public PokemonPanel(PokemonController controller) {
 		super();
 		this.controller = controller;
+		baseLayout = new SpringLayout();
+		pokeBall = new JLabel(new ImageIcon(getClass().getResource("images/pokeBall.png")));
+		
 		setupLayout();
 		setupPanel();
 		setupListeners();
 	}
-	private void setupLayout() {
-		
-	}
 	private void setupPanel() {
+		this.setLayout(baseLayout);
+	}
+	private void setupLayout() {
 		
 	}
 	private void setupListeners() {
